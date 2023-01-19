e4a7b9d26a5fb12d42330b3f75a37d4df1c73cf4
Vinayaga pillai <take2vp@gmail.com>
2023-01-19 22:56:27 +0530

    Level Scene set

---
+++ Assets/Scripts/Editor/HierarchyWindowGroupHeader.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoad]
+public class HierarchyWindowGroupHeader : Editor
+{
+    static HierarchyWindowGroupHeader()
+    {
+        EditorApplication.hierarchyWindowItemOnGUI += HierarchyWindowItemOnGUI;
+    }
+
+    static void HierarchyWindowItemOnGUI(int instanceID, Rect selectionRect)
+    {
+        var gameObject = EditorUtility.InstanceIDToObject(instanceID) as GameObject;
+
+        if (gameObject != null && gameObject.name.StartsWith("---", System.StringComparison.Ordinal))
+        {
+            EditorGUI.DrawRect(selectionRect, Color.gray);
+            EditorGUI.DropShadowLabel(selectionRect, gameObject.name.Replace("-", "").ToUpperInvariant());
+        }
+    }
+}
